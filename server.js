523405a47de942cfbca7ceb64ef14a6ac0e8cd85
J <torres.j.isaias@gmail.com>
2021-08-31 23:45:21 -0500

    added port environment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+
+const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
 
@@ -68,6 +70,6 @@ app.get('/api/animals', (req, res) => {
 
 
 // chain the listen() method onto the server
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`API server now on port 3001!`);
 });
